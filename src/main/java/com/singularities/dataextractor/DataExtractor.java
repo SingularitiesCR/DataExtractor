ea4f8993fb92346acd0bf175f20f6e8996416a84
José Pablo Parajeles Jiménez <jose.parajeles@singularities.com>
2018-02-14 15:48:34 -0600

    Create project structre

---
+++ src/main/java/com/singularities/dataextractor/DataExtractor.java
@@ -0,0 +1,12 @@
+package com.singularities.dataextractor;
+
+public class DataExtractor {
+
+  public String nop(){
+    return "nop";
+  }
+
+  public static void main(String[] args) {
+    System.out.println(new DataExtractor().nop());
+  }
+}
---
+++ src/test/java/com/singularities/dataextractor/DataExtractorTest.java
@@ -0,0 +1,11 @@
+package com.singularities.dataextractor;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataExtractorTest {
+
+  @org.junit.jupiter.api.Test
+  void nop() {
+    assertEquals("nop",new DataExtractor().nop());
+  }
+}
