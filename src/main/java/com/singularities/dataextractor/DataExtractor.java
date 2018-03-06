6e2dffb0ebcc84897ff20caa325704c29ff78ac1
José Pablo Parajeles Jiménez <jose.parajeles@singularities.com>
2018-03-05 20:21:07 -0600

    Test for connection

--- src/main/java/com/singularities/dataextractor/DataExtractor.java
+++ src/main/java/com/singularities/dataextractor/DataExtractor.java
@@ -1,8 +1,32 @@
 package com.singularities.dataextractor;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import java.lang.management.ManagementFactory;
+import java.util.Properties;
+
 public class DataExtractor {
 
-  public static void main(String[] args) {
+  private static final String POSTGRES = "postgres";
+
+  public static void main(String[] args) throws InterruptedException {
     // TODO
+
+    System.out.println(ManagementFactory.getRuntimeMXBean().getName());
+
+    Thread.sleep(20000);
+    Properties connectionProperties = new Properties();
+    connectionProperties.put("user", POSTGRES);
+    connectionProperties.put("password", POSTGRES);
+
+    SparkSession sparkSession = SparkSession.builder().master("local[1]").appName("ss").getOrCreate();
+    sparkSession.sparkContext().setLogLevel("ERROR");
+    Dataset<Row> dataset = sparkSession.read()
+        .jdbc("jdbc:postgresql://localhost:5432/postgres",
+            "test_massive", connectionProperties);
+
+    dataset.show(250000);
   }
 }
