c45e5b81f6442af64bfd3acd82d7557d2065d9e6
José Pablo Parajeles Jiménez <jose.parajeles@singularities.com>
2018-03-14 20:18:54 -0600

    Fix test

--- src/test/java/com/singularities/dataextractor/extractors/XlsxExtractorTest.java
+++ src/test/java/com/singularities/dataextractor/extractors/XlsxExtractorTest.java
@@ -16,51 +16,52 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class XlsxExtractorTest {
 
-    private String filename;
-    private XlsxExtractor extractor;
+  private static final String SHEET1 = "Sheet1";
+  private String filename;
+  private XlsxExtractor extractor;
 
-    private String getFilename(String name) {
-        return Objects.requireNonNull(getClass().getClassLoader().getResource(name)).getPath();
-    }
+  private String getFilename(String name) {
+    return Objects.requireNonNull(getClass().getClassLoader().getResource(name)).getPath();
+  }
 
-    @BeforeEach
-    void setUp() {
-        SparkSession session = SparkSession.builder().master("local[*]").appName("XLSX Test").getOrCreate();
-//        SparkSession.setActiveSession(session);
-        session.sparkContext().setLogLevel("ERROR");
-        extractor = new XlsxExtractor();
-        extractor.setBatchSize(10);
-    }
+  @BeforeEach
+  void setUp() {
+    SparkSession session = SparkSession.builder().master("local[*]").appName("XLSX Test").getOrCreate();
+    session.sparkContext().setLogLevel("ERROR");
+  }
 
-    @Test
-    void load() throws FileNotFoundException {
-        filename = getFilename("data.xlsx");
-        extractor.load(filename, "Sheet1", true);
-        assertNotNull(extractor.getSchema());
-    }
+  @Test
+  void load() throws FileNotFoundException {
+    filename = getFilename("data.xlsx");
+    extractor = new XlsxExtractor.XlsxExtractorBuilder().setBatchSize(10).setFilename(filename)
+        .setSheet(SHEET1).setHasHeader(true).build();
+    assertNotNull(extractor.getSchema());
+  }
 
-    @Test
-    void loadNoHeaders() throws FileNotFoundException {
-        filename = getFilename("noHeaders.xlsx");
-        extractor.load(filename, "Sheet1", false);
-        extractor.nextBatch();
-        StructType schema = extractor.getSchema();
-        assertNotNull(schema);
-        assertEquals(5, schema.fieldNames().length);
-        assertEquals("H0", schema.fieldNames()[0]);
-    }
+  @Test
+  void loadNoHeaders() throws FileNotFoundException {
+    filename = getFilename("noHeaders.xlsx");
+    extractor = new XlsxExtractor.XlsxExtractorBuilder().setBatchSize(10).setFilename(filename)
+        .setSheet(SHEET1).setHasHeader(false).build();
+    extractor.nextBatch();
+    StructType schema = extractor.getSchema();
+    assertNotNull(schema);
+    assertEquals(5, schema.fieldNames().length);
+    assertEquals("H0", schema.fieldNames()[0]);
+  }
 
-    @Test
-    void nextBatch() throws FileNotFoundException {
-        filename = getFilename("data.xlsx");
-        extractor.load(filename, "Sheet1", true);
-        Dataset<Row> batch = extractor.nextBatch();
-        assertNotNull(batch);
-        assertTrue(extractor.batchSize <= batch.count());
-        assertEquals("A", batch.first().get(0));
-        assertEquals("B", extractor.nextBatch().first().get(0));
-        assertEquals("C", extractor.nextBatch().first().get(0));
-        assertEquals(0, extractor.nextBatch().count());
-    }
+  @Test
+  void nextBatch() throws FileNotFoundException {
+    filename = getFilename("data.xlsx");
+    extractor = new XlsxExtractor.XlsxExtractorBuilder().setBatchSize(10).setFilename(filename)
+        .setSheet(SHEET1).setHasHeader(true).build();
+    Dataset<Row> batch = extractor.nextBatch();
+    assertNotNull(batch);
+    assertTrue(extractor.batchSize <= batch.count());
+    assertEquals("A", batch.first().get(0));
+    assertEquals("B", extractor.nextBatch().first().get(0));
+    assertEquals("C", extractor.nextBatch().first().get(0));
+    assertEquals(0, extractor.nextBatch().count());
+  }
 
 }
