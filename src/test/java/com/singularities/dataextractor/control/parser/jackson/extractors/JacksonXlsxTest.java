877c7f3938e8d21d686a610d2ee05c319a0413ae
José Pablo Parajeles <jose.parajeles@singularities.com>
2018-03-22 16:33:14 -0600

    Jackson xlsx model and test

---
+++ src/test/java/com/singularities/dataextractor/control/parser/jackson/extractors/JacksonXlsxTest.java
@@ -0,0 +1,39 @@
+package com.singularities.dataextractor.control.parser.jackson.extractors;
+
+import com.google.common.collect.Lists;
+import com.singularities.dataextractor.extractors.XlsxExtractor;
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+import java.util.Objects;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by aleph on 3/22/18.
+ * Singularities
+ */
+class JacksonXlsxTest {
+
+  @Test
+  void createExtractor() throws FileNotFoundException, SQLException {
+    SparkSession sparkSession = SparkSession.builder().master("local[*]").appName
+        ("JacksonXlsxTest").getOrCreate();
+
+    String path = Objects.requireNonNull(getClass().getClassLoader().getResource("data.xlsx"))
+        .getPath();
+
+    JacksonXlsx jacksonXlsx = new JacksonXlsx();
+    jacksonXlsx.setFilename(path);
+    jacksonXlsx.setBatchSize(200);
+    jacksonXlsx.setHasHeader(true);
+    jacksonXlsx.setSheets(Lists.newArrayList("Sheet1"));
+    XlsxExtractor extractor = jacksonXlsx.createExtractor();
+    Dataset<Row> batch = extractor.nextBatch();
+    String first = batch.first().getString(0);
+    assertEquals("A", first);
+  }
+}
