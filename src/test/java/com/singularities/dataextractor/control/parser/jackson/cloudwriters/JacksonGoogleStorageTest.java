84bbbff2e20084116aed89f82b051fd7cd488cfc
Michael Montano <michael.montano@singularities.com>
2018-04-02 10:25:51 -0600

    Add test for google storage config from a JSON

--- src/test/java/com/singularities/dataextractor/control/parser/jackson/cloudwriters/JacksonGoogleStorageTest.java
+++ src/test/java/com/singularities/dataextractor/control/parser/jackson/cloudwriters/JacksonGoogleStorageTest.java
@@ -1,9 +1,14 @@
 package com.singularities.dataextractor.control.parser.jackson.cloudwriters;
 
 import com.singularities.dataextractor.cloudwriters.GoogleStorageParquetUploader;
+import com.singularities.dataextractor.control.parser.jackson.extractors.JacksonCsv;
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,4 +31,18 @@ class JacksonGoogleStorageTest {
 //    assertEquals(expected, writer); Google Object does not implement equals
     assertNotNull(writer);
   }
+
+  @Test
+  @Ignore
+  void testJsonFile() throws IOException {
+    String path = Objects.requireNonNull(getClass().getClassLoader().getResource("googleStorage.json")).getPath();
+    ObjectMapper mapper = new ObjectMapper();
+    JacksonGoogleStorage storage;
+    storage = mapper.readValue(new File(path), JacksonGoogleStorage.class);
+    GoogleStorageParquetUploader writer = storage.createWriter();
+//    GoogleStorageParquetUploader expected = new GoogleStorageParquetUploader(bucketName, credentialPath);
+//    assertEquals(expected, writer); Google Object does not implement equals
+    assertNotNull(writer);
+  }
+
 }
